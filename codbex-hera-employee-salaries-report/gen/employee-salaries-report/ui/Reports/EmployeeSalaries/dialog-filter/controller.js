c49f621b855bfc970ca2e7758fd4a9644221c2b0
NicoleNG18 <nicole.georgieva18@abv.bg>
2024-10-23 15:27:57 +0300

    add employee salaries report

---
+++ codbex-hera-employee-salaries-report/gen/employee-salaries-report/ui/Reports/EmployeeSalaries/dialog-filter/controller.js
@@ -0,0 +1,50 @@
+angular.module('page', ["ideUI", "ideView"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-hera-employee-salaries-report.Reports.EmployeeSalaries';
+	}])
+	.controller('PageController', ['$scope', 'messageHub', 'ViewParameters', function ($scope, messageHub, ViewParameters) {
+
+		$scope.entity = {};
+		$scope.forms = {
+			details: {},
+		};
+
+		let params = ViewParameters.get();
+		if (Object.keys(params).length) {
+			if (params?.entity?.startdate) {
+				params.entity.startdate = new Date(params.entity.startdate);
+			}
+			$scope.entity = params.entity ?? {};
+			$scope.selectedMainEntityKey = params.selectedMainEntityKey;
+			$scope.selectedMainEntityId = params.selectedMainEntityId;
+		}
+
+		$scope.filter = function () {
+			let entity = $scope.entity;
+			const filter = {
+
+			};
+			if (entity.startdate) {
+				filter.startdate = entity.startdate?.getTime();
+			}
+			messageHub.postMessage("entitySearch", {
+				entity: entity,
+				filter: filter
+			});
+			$scope.cancel();
+		};
+
+		$scope.resetFilter = function () {
+			$scope.entity = {};
+			$scope.filter();
+		};
+
+		$scope.cancel = function () {
+			messageHub.closeDialogWindow("EmployeeSalaries-Report-filter");
+		};
+
+		$scope.clearErrorMessage = function () {
+			$scope.errorMessage = null;
+		};
+
+	}]);
